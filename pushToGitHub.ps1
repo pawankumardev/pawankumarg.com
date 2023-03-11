200651efb780ec6a7b9456806fb806c2f763c2b0
Pawankumar Gurav <pawankumarpgurav@gmail.com>
2023-03-11 16:10:40 +0530

    Changes from Auto Push: 03/11/2023 16:10:39

--- pushToGitHub.ps1
+++ pushToGitHub.ps1
@@ -1,1 +1,22 @@
 echo "Starting"
+
+Remove-Item "E:\PawankumarG.com\dev\Github\*" -exclude "E:\PawankumarG.com\dev\Github\.git"
+echo "1 moment.."
+sleep(2)
+echo "removed"
+Copy-Item -Recurse "E:\PawankumarG.com\dev\Green_PawankumarG\_site\*" "E:\PawankumarG.com\dev\Github\"
+echo "copied"
+
+echo "starting Git operations.."
+
+cd E:
+
+cd E:\PawankumarG.com\dev\Github\
+
+git add * --force
+
+git commit -m "Changes from Auto Push: $(Get-Date)"
+
+git push origin main
+
+echo "Completed..."
---
+++ startServer.ps1
@@ -0,0 +1,2 @@
+cd E:\PawankumarG.com\dev\Green_PawankumarG\
+bundle exec jekyll serve
